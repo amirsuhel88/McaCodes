1c7211a9716788dcb834e06ff2deed3011bf6ac5
saddamsegwitz <saddam@segwitz.com>
2022-09-26 23:01:38 +0530

    fibonacci series to a given range.

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Fibonacci {
+    public static void main(String []args) throws Exception {
+        int range, temp, a = 0, b = 1, c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the how many terms: ");
+        range = Integer.parseInt(br.readLine());
+        for (int i = 0; i < range; i++) {
+            System.out.println("" + a);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
