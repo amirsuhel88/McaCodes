ea470c76d6966a33394460e7c59e11092742d34b
Amir Suhel <amirsuhel88@gmail.com>
2022-09-29 17:53:05 +0530

    use of copy constructor

---
+++ Trying.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Student
+{
+    String name;
+    int roll;
+    Student(String a, int b)
+    {
+        name = a;
+        roll = b;
+    }
+    Student(Student info)
+    {
+        name = info.name;
+        roll = info.roll;
+    }
+    void display()
+    {
+        System.out.println("your name is: " + name);
+        System.out.println("Your roll no is: " + roll);
+    }
+}
+
+public class Trying
+{
+    public static void main(String[] args)
+    {
+        String sname;
+        int sroll;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        sname = sc.nextLine();
+        System.out.println("Enter your roll no: ");
+        sroll = sc.nextInt();
+        Student st1 = new Student(sname, sroll);
+        Student st2 = new Student(st1);
+        st2.display();
+    }
+}
