f50866bfb7bd75ae015bc26328fad4c19d580319
amirsuhel88 <amirsuhel88@gmail.com>
2022-10-04 23:42:58 +0530

    Scanner is closed

--- SumOfArrey.java
+++ SumOfArrey.java
@@ -1,18 +1,21 @@
+
 import java.util.Scanner;
 
 public class SumOfArrey {
-        public static void main(String[] args){
-            int n,sum=0;
+        public static void main(String[] args) 
+        {
             Scanner sc=new Scanner(System.in);
+            int n,sum=0;
             System.out.println("Enter How many terms: ");
             n= sc.nextInt();
             int[] array=new int[10];
             System.out.println("Enter the values: ");
             for(int i=0;i<n;i++)
             {
-                array[i]=sc.nextInt();
+                array[i]=sc.nextInt();                    
                 sum= sum + array[i];
             }
             System.out.println("The sum of the array is: "+sum);
+            sc.close();
         }
 }
