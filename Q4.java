f27a9023ecc11637ab168990c6c6f6ca30966fbd
suhel <amirsuhel88@gmail.com>
2022-12-08 18:09:43 +0530

    working

--- Q4.java
+++ Q4.java
@@ -1,5 +1,26 @@
 //Write a program to demonstrate use of nested class
-
-public class Q4 {
-    
+class GIMTStudent
+ {
+    int roll_no=12;
+    String name = "Amir";
+    private class MCAStudent
+    {
+        void profile()
+        {
+            System.out.println("Name is: "+name+"and Roll no is: "+roll_no);
+        }
+    }
+    void display()
+    {
+        MCAStudent ob1 = new MCAStudent();
+        ob1.profile();
+    }
+}
+public class Q4
+{
+    public static void main(String[] args)
+    {
+        GIMTStudent ob = new GIMTStudent();
+        ob.display();
+    }
 }
